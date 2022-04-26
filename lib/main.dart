1fca39044b7f3eadc7d3a6a8102868f5d9863d48
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-04-26 15:38:51 +0700

    Pembuatan halaman category title

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_delivery_app/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Food App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,6 +26,15 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+        scaffoldBackgroundColor: kWhiteColor,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          button: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
       ),
       home: const HomeScreen(),
     );
@@ -39,6 +51,85 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 20, top: 20),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: SvgPicture.asset(
+                  'assets/icons/menu.svg',
+                  height: 11,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                'Simple way to order \nTasty food',
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  right: 10,
+                ),
+                child: Row(
+                  children: const <Widget>[
+                    CategoryTitle(
+                      title: 'Semua',
+                      isActive: true,
+                    ),
+                    CategoryTitle(
+                      title: 'Ayam',
+                    ),
+                    CategoryTitle(
+                      title: 'Nasi',
+                    ),
+                    CategoryTitle(
+                      title: 'Minuman',
+                    ),
+                    CategoryTitle(
+                      title: 'Dessert',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTitle extends StatelessWidget {
+  final String title;
+  final bool isActive;
+
+  const CategoryTitle({
+    Key? key,
+    required this.title,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 18),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.button?.copyWith(
+              color: isActive ? kPrimaryColor : kTextColor.withOpacity(0.4),
+            ),
+      ),
+    );
   }
 }
